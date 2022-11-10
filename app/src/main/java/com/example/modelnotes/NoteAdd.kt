b10183d3b0e73fc19c44fc0e4b22e003336f84f5
L <leonardo.alvirubro@hotmail.com>
2022-11-10 11:51:54 -0300

    style: Screen design completed

---
+++ app/src/main/java/com/example/modelnotes/NoteAdd.kt
@@ -0,0 +1,11 @@
+package com.example.modelnotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteAdd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add)
+    }
+}
