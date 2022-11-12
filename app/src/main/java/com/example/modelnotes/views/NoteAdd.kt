83bce192f1370092071de3544ee9b59fa91f43f1
L <leonardo.alvirubro@hotmail.com>
2022-11-11 23:13:49 -0300

    feat: NoteAdd class 'completed'

--- app/src/main/java/com/example/modelnotes/views/NoteAdd.kt
+++ app/src/main/java/com/example/modelnotes/views/NoteAdd.kt
@@ -1,12 +1,69 @@
 package com.example.modelnotes.views
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.modelnotes.R
+import com.example.modelnotes.databinding.ActivityNoteAddBinding
+import com.example.modelnotes.model.Note
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.logging.SimpleFormatter
 
 class NoteAdd : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNoteAddBinding
+    private lateinit var note: Note
+    private lateinit var oldNote: Note
+
+    var isUpdateBoolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_add)
+
+
+
+        binding = ActivityNoteAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        try {
+            oldNote = intent.getSerializableExtra("current_note") as Note
+            binding.etTitle.setText(oldNote.title)
+            isUpdateBoolean = true
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        binding.tvCheckFinish.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val noteDesc = binding.etDesc.text.toString()
+
+            if (title.isNotEmpty() || noteDesc.isNotEmpty()) {
+                val formatter = SimpleDateFormat("EEE, d MMM yyyy HH:mm a")
+
+                if (isUpdateBoolean) {
+                    note = Note(
+                        oldNote.id, title, noteDesc, formatter.format(Date())
+                    )
+                } else {
+                    note = Note(
+                        null, title, noteDesc, formatter.format(Date())
+                    )
+                }
+
+                val intent = Intent()
+                intent.putExtra("note", note)
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            } else {
+                Toast.makeText(this@NoteAdd, "We need data", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
+        binding.imgBackArrowIcon.setOnClickListener {
+            onBackPressed()
+        }
+
     }
 }
